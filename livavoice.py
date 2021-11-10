483111d52b11194babb693571ff5e986625baa73
Prashanth Kamath <prashlkam@gmail.com>
2021-11-10 18:02:09 +0530

    refactored code in livavoice.py.....

--- livavoice.py
+++ livavoice.py
@@ -23,88 +23,109 @@ class LivaVoice():
 	def take_command(self):
 		try:
 			with sr.Microphone() as source:
-				# print('listening...')
+				print('listening...')
 				voice = listener.listen(source)
 				command = listener.recognize_google(voice)
 				command = command.lower()
 				if 'liva' in command:
-					command = command.replace('liva', '')
-					# print(command)
+					command = command.replace('liva ', '')
+					print(command)
 		except:
 			pass
-		self.outputtext.setText(command)
+		# self.outputtext.setText(command)
 		return command
 	
 	def liva_run(self):
 		command = take_command()
+		resulttoshow = ''
 		# print(command)
 		if 'run' in command:
-			cmdtorun = command.replace('run', '')
-			exec_cmd(cmdtorun)
+			cmdtorun = command.replace('run ', '')
+			# exec_cmd(cmdtorun)
 		elif 'launch' in command:
-			cmdtorun = command.replace('launch', '')
-			exec_cmd(cmdtorun)
+			cmdtorun = command.replace('launch ', '')
+			# exec_cmd(cmdtorun)
 		elif 'play' in command:
 			song = command.replace('play ', '')
 			talk('playing ' + song)
 			pywhatkit.playonyt(song)
 		elif 'who is' in command:
-			person = command.replace('who is', '')
+			person = command.replace('who is ', '')
 			info = wikipedia.summary(person, 3)
 			# print(info)
-			self.outputtext.setText(info)
+			# self.outputtext.setText(info)
+			resulttoshow = info
 			talk(info)
 		elif 'what is' in command:
-			thing = command.replace('what is', '')
+			thing = command.replace('what is ', '')
 			info = wikipedia.summary(thing, 3)
 			# print(info)
-			self.outputtext.setText(info)
+			# self.outputtext.setText(info)
+			resulttoshow = info
 			talk(info)
 		elif 'where is' in command:
-			place = command.replace('where is', '')
+			place = command.replace('where is ', '')
 			info = wikipedia.summary(place, 3)
 			# print(info)
-			self.outputtext.setText(info)
+			# self.outputtext.setText(info)
+			resulttoshow = info
 			talk(info)
 		elif 'how to' in command:
-			qstn = command.replace('how to', '')
-			QMessageBox.about(self, "Alert", 'searching how to ' + qstn)
+			qstn = command.replace('how to ', '')
+			# QMessageBox.about(self, "Alert", 'searching how to ' + qstn)
 			talk('searching how to ' + qstn)
 			pywhatkit.search(qstn)
 		elif 'get information' in command:
-			qstn = command.replace('get information', '')
-			QMessageBox.about(self, "Alert", 'Getting information: ' + qstn)
+			qstn = command.replace('get information ', '')
+			# QMessageBox.about(self, "Alert", 'Getting information: ' + qstn)
 			talk('getting information ' + qstn)
 			pywhatkit.search(qstn)
 		elif 'search information' in command:
-			qstn = command.replace('search information', '')
-			QMessageBox.about(self, "Alert", 'Searching information: ' + qstn)
+			qstn = command.replace('search information ', '')
+			# QMessageBox.about(self, "Alert", 'Searching information: ' + qstn)
 			talk('searching information ' + qstn)
 			pywhatkit.search(qstn)
 		elif 'get info' in command:
-			qstn = command.replace('get info', '')
-			QMessageBox.about(self, "Alert", 'Getting info: ' + qstn)
+			qstn = command.replace('get info ', '')
+			# QMessageBox.about(self, "Alert", 'Getting info: ' + qstn)
 			talk('getting info ' + qstn)
 			pywhatkit.info(qstn)
 		elif 'search info' in command:
-			qstn = command.replace('search info', '')
-			QMessageBox.about(self, "Alert", 'Searching info: ' + qstn)
+			qstn = command.replace('search info ', '')
+			# QMessageBox.about(self, "Alert", 'Searching info: ' + qstn)
 			talk('searching info ' + qstn)
 			pywhatkit.info(qstn)
 		elif 'time' in command:
 			time = datetime.datetime.now().strftime('%I:%M %p')
-			QMessageBox.about(self, "Alert", 'Current time: ' + time)
+			# QMessageBox.about(self, "Alert", 'Current time: ' + time)
+			resulttoshow = 'Current time: ' + time
 			talk('Current time is ' + time)
 		elif 'date' in command:
 			date = datetime.datetime.now().strftime('%d %B %Y')
-			QMessageBox.about(self, "Alert", 'Current date: ' + date)
+			# QMessageBox.about(self, "Alert", 'Current date: ' + date)
+			resulttoshow = 'Current date: ' + date
 			talk('Current date is ' + date)
-		elif 'liva are you single' in command:
-			talk('I am in a relationship with Tux...')
 		elif 'joke' in command:
 			joke = pyjokes.get-joke()
-			self.outputtext.setText(joke)
+			# self.outputtext.setText(joke)
+			resulttoshow = joke
 			talk(joke)
+		elif 'liva are you single' in command:
+			resulttoshow = 'I am in a relationship with Tux...'
+			talk('I am in a relationship with Tux...')
 		else:
-			QMessageBox.about(self, "Alert", 'I didn\'t get you. Please say the command again...')
+			# QMessageBox.about(self, "Alert", 'I didn\'t get you. Please say the command again...')
+			resulttoshow = 'I didn\'t get you. Please say the command again...'
 			talk('I didn\'t get you. Please say the command again...')
+		
+		print('Command:\n--------\n' + command)
+		if resulttoshow == '':
+			resulttoshow = 'No Results to Show Here...'
+		print('Result:\n--------\n' + resulttoshow)
+		return command, resulttoshow
+		
+
+# main
+lv = LivaVoice()
+
+lv.liva_run()
