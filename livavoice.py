e5eb389f8b7d6bd132455f1a65754100f1805a37
Prashanth Kamath <prashlkam@gmail.com>
2021-11-11 13:54:06 +0530

    made changes to livavoice.py - brought it up to a working state...

--- livavoice.py
+++ livavoice.py
@@ -9,18 +9,19 @@ import os
 import sys
 
 class LivaVoice():
-	def __init__():
-		listener = sr.Recognizer()
-		engine = pyttsx3.init()
-		voices = engine.getProperty('voices')
-		engine.setProperty('voice', voices[1].id)
+	def __init__(self):
+		self.listener = sr.Recognizer()
+		self.engine = pyttsx3.init()
+		self.voices = self.engine.getProperty('voices')
+		self.engine.setProperty('voice', self.voices[0].id)
 		
-	def talk(text):
-		engine.say(text)
-		engine.runAndWait()
+	def talk(self, text):
+		self.engine.say(text)
+		self.engine.runAndWait()
 	
 	
 	def take_command(self):
+		command = ''
 		try:
 			with sr.Microphone() as source:
 				print('listening...')
@@ -36,7 +37,8 @@ class LivaVoice():
 		return command
 	
 	def liva_run(self):
-		command = take_command()
+		command = ''
+		command = self.take_command()
 		resulttoshow = ''
 		# print(command)
 		if 'run' in command:
@@ -47,7 +49,7 @@ class LivaVoice():
 			# exec_cmd(cmdtorun)
 		elif 'play' in command:
 			song = command.replace('play ', '')
-			talk('playing ' + song)
+			self.talk('playing ' + song)
 			pywhatkit.playonyt(song)
 		elif 'who is' in command:
 			person = command.replace('who is ', '')
@@ -55,68 +57,68 @@ class LivaVoice():
 			# print(info)
 			# self.outputtext.setText(info)
 			resulttoshow = info
-			talk(info)
+			self.talk(info)
 		elif 'what is' in command:
 			thing = command.replace('what is ', '')
 			info = wikipedia.summary(thing, 3)
 			# print(info)
 			# self.outputtext.setText(info)
 			resulttoshow = info
-			talk(info)
+			self.talk(info)
 		elif 'where is' in command:
 			place = command.replace('where is ', '')
 			info = wikipedia.summary(place, 3)
 			# print(info)
 			# self.outputtext.setText(info)
 			resulttoshow = info
-			talk(info)
+			self.talk(info)
 		elif 'how to' in command:
 			qstn = command.replace('how to ', '')
 			# QMessageBox.about(self, "Alert", 'searching how to ' + qstn)
-			talk('searching how to ' + qstn)
+			self.talk('searching how to ' + qstn)
 			pywhatkit.search(qstn)
 		elif 'get information' in command:
 			qstn = command.replace('get information ', '')
 			# QMessageBox.about(self, "Alert", 'Getting information: ' + qstn)
-			talk('getting information ' + qstn)
+			self.talk('getting information ' + qstn)
 			pywhatkit.search(qstn)
 		elif 'search information' in command:
 			qstn = command.replace('search information ', '')
 			# QMessageBox.about(self, "Alert", 'Searching information: ' + qstn)
-			talk('searching information ' + qstn)
+			self.talk('searching information ' + qstn)
 			pywhatkit.search(qstn)
 		elif 'get info' in command:
 			qstn = command.replace('get info ', '')
 			# QMessageBox.about(self, "Alert", 'Getting info: ' + qstn)
-			talk('getting info ' + qstn)
+			self.talk('getting info ' + qstn)
 			pywhatkit.info(qstn)
 		elif 'search info' in command:
 			qstn = command.replace('search info ', '')
 			# QMessageBox.about(self, "Alert", 'Searching info: ' + qstn)
-			talk('searching info ' + qstn)
+			self.talk('searching info ' + qstn)
 			pywhatkit.info(qstn)
 		elif 'time' in command:
 			time = datetime.datetime.now().strftime('%I:%M %p')
 			# QMessageBox.about(self, "Alert", 'Current time: ' + time)
 			resulttoshow = 'Current time: ' + time
-			talk('Current time is ' + time)
+			self.talk('Current time is ' + time)
 		elif 'date' in command:
 			date = datetime.datetime.now().strftime('%d %B %Y')
 			# QMessageBox.about(self, "Alert", 'Current date: ' + date)
 			resulttoshow = 'Current date: ' + date
-			talk('Current date is ' + date)
+			self.talk('Current date is ' + date)
 		elif 'joke' in command:
 			joke = pyjokes.get-joke()
 			# self.outputtext.setText(joke)
 			resulttoshow = joke
-			talk(joke)
+			self.talk(joke)
 		elif 'liva are you single' in command:
 			resulttoshow = 'I am in a relationship with Tux...'
-			talk('I am in a relationship with Tux...')
+			self.talk('I am in a relationship with Tux...')
 		else:
 			# QMessageBox.about(self, "Alert", 'I didn\'t get you. Please say the command again...')
-			resulttoshow = 'I didn\'t get you. Please say the command again...'
-			talk('I didn\'t get you. Please say the command again...')
+			resulttoshow = ''
+			self.talk('I didn\'t get you. Please say the command again...')
 		
 		print('Command:\n--------\n' + command)
 		if resulttoshow == '':
